bc4e5a49d705f6fc20d4c3917557c7a132e60306
computermad93 <c.murrell93@live.co.uk>
2014-02-21 19:02:42 +0000

    update
    update to code for use with my pc not vm machie

--- listOfSwimmersScript.js
+++ listOfSwimmersScript.js
@@ -10,7 +10,7 @@ done();
 }
 
 function updates() {
-$.getJSON("http://192.168.79.128/test/fetch.php", function(data) {
+$.getJSON("http://192.168.1.83/smallt/fetch.php", function(data) {
 $("#listofswimmers").empty();
 $.each(data.result, function(){
 $("#listofswimmers").append("Name: "+this['name']+"Age: "+this['age']);
